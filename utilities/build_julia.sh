ba07895c027756694970912657aeaba7257bfe91
Zentrik <Zentrik@users.noreply.github.com>
2024-07-28 14:54:37 +0100

    bisect

--- utilities/build_julia.sh
+++ utilities/build_julia.sh
@@ -59,7 +59,7 @@ if [[ ! -z "${USE_JULIA_PGO_LTO_BOLT-}" ]]; then
 
     echo "--- Build Julia Stage 1 - with instrumentation"
 
-    git checkout -b new_branch d9a0c2553ba023adc54a297fce8cb3dc127be8e6
+    git checkout -b new_branch 5a5624c19b66b4d491cb85cd8ed3301ed40338e8
 
     cd contrib/pgo-lto
     ${MAKE} "${MFLAGS[@]}" stage1
